f791c569a3aa90086d9bdf5e060adf4888f62597
Peeters, Stefano <stefano.peeters@wolterskluwer.com>
2022-02-04 13:08:42 +0100

    feat: adding labels to points

--- public/admin/admin-components/InteractiveImageComponent.js
+++ public/admin/admin-components/InteractiveImageComponent.js
@@ -1,8 +1,19 @@
 const Control = createClass({
   valueCache: new Map(),
   imageUploaded: false,
+  ref: 'iimg-ref',
   imageUrl: '',
+  imageWidth: 0,
+  imageHeight: 0,
   pointsLength: 0,
+
+  componentWillUpdate() {
+    if (this.imageUploaded && !!this.refs[this.ref]) {
+      const { width, height } = this.refs[this.ref].getBoundingClientRect();
+      this.imageWidth = width;
+      this.imageHeight = height;
+    }
+  },
   handleImageInput(evt) {
     const [file] = evt.target.files;
     if (file) {
@@ -16,57 +27,80 @@ const Control = createClass({
   handleRemoveImage() {
     this.imageUrl = '';
     this.imageUploaded = false;
+    this.valueCache.clear();
     this.forceUpdate();
+  },
+  handlePointMetaInput(e, key) {
+    if (this.valueCache.has(key)) {
+      this.valueCache.set(key, {
+        ...this.valueCache.get(key),
+        metaData: {
+          tooltip: e.target.value
+        }
+      });
+    }
+    this.triggerChangeUpdate(this.props.value.image, { points: this.valueCache });
   },
   handleImageClick(e) {
     if (!e) return;
     const rect = e.target.getBoundingClientRect();
+
     const x = e.clientX - rect.left;
     const y = e.clientY - rect.top;
     const key = `point-${this.valueCache.size}`;
 
+    const xPercentage = `${(x / rect.width) * 100}%`;
+    const yPercentage = `${(y / rect.height) * 100}%`;
+
     // save state
-    this.valueCache.set(key, { ...this.valueCache.get(key), x, y });
+    this.valueCache.set(key, { ...this.valueCache.get(key), x: xPercentage, y: yPercentage });
     // update cms value
+    this.triggerChangeUpdate({}, { points: this.valueCache });
+  },
+  triggerChangeUpdate(imageData, pointsData) {
     this.props.onChange({
-      image: this.imageUrl,
-      points: this.valueCache
+      image: {
+        ...imageData
+      },
+      ...pointsData
+    });
+  },
+  renderPoints() {
+    return Array.from(this.valueCache.entries()).map(point => {
+      const key = point[0];
+      const value = point[1];
+      const styleObject = {};
+      const style = {
+        ...this.setPointStyles(styleObject),
+        ...this.setPointPosition(styleObject, value)
+      };
+      return h(
+        'span',
+        {
+          'data-key': key,
+          style
+        },
+        ''
+      );
     });
   },
   renderImage() {
+    const randomId = '_' + Math.random().toString(36).substring(4, 9);
     const args = {
       img: {
+        id: randomId,
+        ref: 'iimg-ref', // sets ref
         src: this.imageUrl,
+        className: 'iimg-image',
         onClick: this.handleImageClick
       },
       button: {
         onClick: this.handleRemoveImage
       }
     };
-    const points = () => {
-      const pointsArray = Array.from(this.valueCache.entries());
 
-      return pointsArray.map(point => {
-        const key = point[0];
-        const value = point[1];
-        const styleObject = {};
-        const style = {
-          ...this.setPointStyles(styleObject),
-          ...this.setPointPosition(styleObject, value)
-        };
-
-        return h(
-          'span',
-          {
-            'data-key': key,
-            style
-          },
-          ''
-        );
-      });
-    };
     return h('div', { className: 'iimg-image-wrapper' }, [
-      h('div', null, [h('img', args.img), h('div', {}, points())]),
+      h('div', { className: 'iimg-points-wrapper' }, [this.renderPoints(), h('img', args.img)]),
       h('button', args.button, `Remove image`)
     ]);
   },
@@ -77,6 +111,17 @@ const Control = createClass({
       onInput: this.handleImageInput
     };
     return h('input', args);
+  },
+  renderPointsMetaData() {
+    const args = {
+      type: 'text',
+      style: {
+        border: '1px solid black'
+      }
+    };
+    return Array.from(this.valueCache.keys()).map(key => {
+      return [h('span', {}, key), h('input', { ...args, onInput: e => this.handlePointMetaInput(e, key) })];
+    });
   },
   setPointStyles(styleObject) {
     styleObject.position = 'absolute';
@@ -89,12 +134,16 @@ const Control = createClass({
     return styleObject;
   },
   setPointPosition(styleObject, { x, y }) {
-    styleObject.left = `${x}px`;
-    styleObject.top = `${y}px`;
+    styleObject.left = `${x}`;
+    styleObject.top = `${y}`;
     return styleObject;
   },
   render: function () {
-    return h('div', null, [this.imageUploaded ? this.renderImage() : this.renderFileInput()]);
+    return h('div', null, [
+      this.imageUploaded
+        ? [this.renderImage(), this.valueCache.size !== 0 ? this.renderPointsMetaData() : null]
+        : this.renderFileInput()
+    ]);
   }
 });
 
